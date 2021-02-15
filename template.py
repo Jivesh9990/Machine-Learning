638f82952cb76e6e06b3be5c694ba10d1586e42e
Jivesh Tulshyan <jivesh9990@gmail.com>
2021-02-15 22:16:35 +0530

    commit

--- template.py
+++ template.py
@@ -1,13 +1,19 @@
+# common 
 import pandas as pd
 import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
 
-# ML based import
-
+# ml preprocessing
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
+
+# algo
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.neighbors import KNeighborsRegressor
